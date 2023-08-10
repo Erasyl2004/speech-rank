0b95d7d4fff6b961c34d7138fe74fefee58b0136
Erasyl2004 <nyt20192004@gmail.com>
2023-08-10 18:06:43 +0600

    newfriend3

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -198,10 +198,10 @@ export default function LandingPage (){
                               <p className="text-[#666666] text-2xl">This application will help you to make technical<br/>examination and analysis according to the rules<br/>of your calls.</p>
                          </div>
                          <div className="flex justify-center w-full">
-                              <button className="font-mono font-medium text-white text-lg bg-[#4169E1] text-center rounded-lg w-36 h-12">Try Now</button>
+                              <button className="font-mono font-medium text-white text-lg bg-[#4169E1] text-center rounded-lg w-36 h-12" onClick={TryNowButton}>Try Now</button>
                          </div>
                     </div>
-                    <div className="w-full pt-24" >
+                    <div className="flex justify-center w-screen pt-24 pb-36" >
                               <div className="bg-[#F7F9FB] drop-shadow-lg rounded-2xl w-[69rem] h-[20rem] flex justify-center flex-wrap">
                                    <div className="w-full font-mono text-2xl md: font-bold text-left flex justify-center">
                                         <div className="pt-4">
@@ -209,38 +209,63 @@ export default function LandingPage (){
                                              <p className="text-[#4169E1]">we use for your calls?</p>
                                         </div>
                                    </div>
-                              <div className="space-x-8 flex justify-center flex-wrap">
-                                        <div className="font-mono bg-[#FFFFFF] w-[21rem] h-[10rem] rounded-2xl drop-shadow-lg md: font-bold">
+                              <div className="space-x-2 flex justify-center flex-wrap">
+                                        <div className="font-mono bg-[#FFFFFF] w-[19.5rem] h-[8.5rem] rounded-2xl drop-shadow-lg md: font-bold">
                                                   <div className="flex justify-center pt-2 items-center space-x-4">
                                                        <div className="rounded-full w-[2.5rem] h-[2.5rem] bg-[#4169E1] flex items-center justify-center">
                                                             <p  className="text-white font-mono font-bold text-3xl">1</p>
                                                        </div>
                                                        <p className="text-[#222222] text-center text-xl ">Voice recognition</p>
                                                   </div>
-                                                  <p className="text-[#666666] pt-4 text-center">With the help of AI, we determine<br/>people by the timbre of the voice</p>
+                                                  <p className="text-[#666666] pt-4 text-center text-sm">With the help of AI, we determine<br/>people by the timbre of the voice</p>
                                         </div>
-                                             <div className="font-mono bg-[#FFFFFF] w-[21rem] h-[10rem] rounded-2xl drop-shadow-lg md: font-bold">
+                                             <div className="font-mono bg-[#FFFFFF] w-[19.5rem] h-[8.5rem] rounded-2xl drop-shadow-lg md: font-bold">
                                                   <div className="flex justify-center pt-2 items-center space-x-4">
                                                        <div className="rounded-full w-[2.5rem] h-[2.5rem] bg-[#4169E1] flex items-center justify-center">
                                                             <p  className="text-white font-mono font-bold text-3xl">2</p>
                                                        </div>
                                                        <p className="text-[#222222] text-center text-xl ">Reglament</p>
                                                   </div>
-                                                  <p className="text-[#666666] pt-4 text-center">We use the regulations of advanced organizations to evaluate the work of a manager</p>
+                                                  <p className="text-[#666666] pt-4 text-center text-sm">We use the regulations of advanced organizations to evaluate the work of a manager</p>
                                              </div>
-                                             <div className="font-mono bg-[#FFFFFF] w-[21rem] h-[10rem] rounded-2xl drop-shadow-lg md: font-bold">
+                                             <div className="font-mono bg-[#FFFFFF] w-[19.5rem] h-[8.5rem] rounded-2xl drop-shadow-lg md: font-bold">
                                                   <div className="flex justify-center pt-2 items-center space-x-4">
                                                        <div className="rounded-full w-[2.5rem] h-[2.5rem] bg-[#4169E1] flex items-center justify-center">
                                                             <p  className="text-white font-mono font-bold text-3xl">3</p>
                                                        </div>
                                                        <p className="text-[#222222] text-center text-xl ">VectorDB</p>
                                                   </div>
-                                                  <p className="text-[#666666] pt-4 text-center">We use a vector database to identify technical errors</p>
+                                                  <p className="text-[#666666] pt-4 text-center text-sm">We use a vector database to identify technical errors</p>
                                              </div>
                                    </div>
                               </div>
                          </div>
-
+                         <footer className="pt-24 footer p-10 text-base-content bg-[#F7F9FB]">
+                              <div>
+                                   <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+                                   <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+                              </div> 
+                              <div>
+                                   <span className="footer-title">Services</span> 
+                                   <a className="link link-hover">Branding</a> 
+                                   <a className="link link-hover">Design</a> 
+                                   <a className="link link-hover">Marketing</a> 
+                                   <a className="link link-hover">Advertisement</a>
+                              </div> 
+                              <div>
+                                   <span className="footer-title">Company</span> 
+                                   <a className="link link-hover">About us</a> 
+                                   <a className="link link-hover">Contact</a> 
+                                   <a className="link link-hover">Jobs</a> 
+                                   <a className="link link-hover">Press kit</a>
+                              </div> 
+                              <div>
+                                   <span className="footer-title">Legal</span> 
+                                   <a className="link link-hover">Terms of use</a> 
+                                   <a className="link link-hover">Privacy policy</a> 
+                                   <a className="link link-hover">Cookie policy</a>
+                              </div>
+                         </footer>
           </div>
        </>
     )
